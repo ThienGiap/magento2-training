8f0a61a54349a0b4db01555dfd69d21eaeebfcc1
ThienGiap <nguyenthiengiap04@gmail.com>
2025-02-03 11:42:55 +0700

    initial commit

---
+++ app/code/Valley/Bai1/Block/Weather.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Valley\Bai1\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class Weather extends Template
+{
+    protected function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+}
